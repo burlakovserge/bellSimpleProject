5a0de8dfa6b8aa7eac4deca008e8b7f4dc96e59a
burlakov.serge <burlakov.serge@outlook.com>
2020-07-08 15:20:39 +0400

    add in organization controller /update, /save upd

--- src/main/java/ru/bellintegrator/practice/controller/OrganizationController.java
+++ src/main/java/ru/bellintegrator/practice/controller/OrganizationController.java
@@ -1,12 +1,14 @@
 package ru.bellintegrator.practice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import ru.bellintegrator.practice.dto.OrganizationDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.bellintegrator.practice.dto.organization.request.OrganizationRequestDto;
+import ru.bellintegrator.practice.dto.organization.response.OrganizationResponseDto;
 import ru.bellintegrator.practice.service.OrganizationService;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api/organization")
@@ -15,7 +17,19 @@ public class OrganizationController {
     OrganizationService service;
 
     @GetMapping("/{id}")
-    public OrganizationDto getOrgById (@PathVariable("id") Integer id){
+    public OrganizationResponseDto getOrgById(@PathVariable("id") Integer id) {
         return service.findById(id);
+    }
+
+    @PostMapping("/update")
+    public ResponseEntity updateOrganization(@Valid @RequestBody OrganizationRequestDto organization) {
+        service.update(organization);
+        return new ResponseEntity<>("{result: success}", HttpStatus.OK);
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity saveOrganization(@Valid @RequestBody OrganizationRequestDto organization) {
+        service.save(organization);
+        return new ResponseEntity<>("{result: success}", HttpStatus.OK);
     }
 }
